0128f0619d365cd7e4d427d50d05787146980f69
dangcntt19 <dangzip6@mail.com>
2023-03-17 09:23:54 +0700

    xong crud

--- src/Controllers/homeController.js
+++ src/Controllers/homeController.js
@@ -1,5 +1,8 @@
 import { json } from "body-parser";
 import db from "../models/index";
+import { where } from "sequelize";
+import CRUDService from "../servers/CRUDService";
+// import { adjacent } from "sequelize/types/operators";
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
@@ -11,6 +14,62 @@ let getHomePage = async (req, res) => {
     console.log("e", e);
   }
 };
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.render("crud.ejs");
+};
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  console.log(
+    "----------------------------------------------------------------"
+  );
+  console.log(data);
+  console.log(
+    "----------------------------------------------------------------"
+  );
+  return res.render("displayCRUD.ejs", {
+    dataTable: data,
+  });
+};
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserById(userId);
+    //check user not found
+
+    return res.render("editCRUD.ejs", { user: userData });
+  } else {
+    // console.log(req.query.id);
+    return res.send("user not found");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  await CRUDService.updateUserData(data);
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("displayCRUD.ejs", {
+    dataTable: allUsers,
+  });
+};
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserByID(id);
+    return res.send(" xoa thanh cong");
+  } else {
+    return res.send(" user k cos");
+  }
+};
 module.exports = {
   getHomePage: getHomePage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+  displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,16 @@
 import express from "express";
+import { where } from "sequelize";
 import homeCotroller from "../Controllers/homeController";
 let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeCotroller.getHomePage);
-  router.get("/dang", (req, res) => {
-    return res.send("hihihhihi");
-  });
+  router.get("/CRUD", homeCotroller.getCRUD);
+  router.post("/post-crud", homeCotroller.postCRUD);
+  router.get("/get-crud", homeCotroller.displayGetCRUD);
+  router.get("/edit-crud", homeCotroller.getEditCRUD);
+  router.post("/put-crud", homeCotroller.putCRUD);
+  router.get("/delete-crud", homeCotroller.deleteCRUD);
+  //rest api
   //rest api
   return app.use("/", router);
 };
